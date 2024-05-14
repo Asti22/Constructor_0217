2b4d7013d5af62e58d82e09c58e9a9cd654dcfb8
Asti22 <uun3461@gmail.com>
2024-05-14 15:54:16 +0700

    membuat contruktor

--- contruktor.cpp
+++ contruktor.cpp
@@ -11,9 +11,41 @@ public:
    Mahasiswa()
    {
     nim =0;
-    nama= "";
+    nama= " ";
    }; 
    Mahasiswa(int iNim) {
     nim =iNim;
-   }
+   };
+   
+
+
+Mahasiswa(string iNama)
+ 
+{
+    nama = iNama;
 };
+
+Mahasiswa(int iNim,string iNama){
+    nim =iNim;
+    nama =iNama;
+};
+void cetak(){
+    cout <<"Nim =" <<nim <<endl;
+    cout <<"Nama =" <<nama <<endl;
+    cout <<endl;
+};
+};
+
+int main(){
+    Mahasiswa mhs1;
+    Mahasiswa mhs2(20);
+    Mahasiswa mhs3("Asti");
+    Mahasiswa mhs4(34, "Aldys");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
+}
